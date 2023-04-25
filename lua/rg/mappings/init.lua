1db8208bda7cd5345c6c52caf3b675a1d26ea00f
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-04-25 05:07:57 +0200

    brought back <leader>f for reindenting

--- lua/rg/mappings/init.lua
+++ lua/rg/mappings/init.lua
@@ -8,9 +8,15 @@ vim.keymap.set("n", "<leader>WW", ":w!<cr>");
 vim.keymap.set("n", "<leader>gt", vim.cmd.LazyGit);
 vim.keymap.set("i", "jj", "<ESC>");
 
+-- Yank to Clipboard
+vim.keymap.set("v", '<leader>y', '"+y');
+vim.keymap.set("n", '<leader>y', '"+yy');
+
+-- Paste in visual mode without resetting paste buffer content 
+vim.keymap.set("x", "<leader>p", '"_dP')
+
 -- Auto Indent
--- vim.keymap.set("n", "<leader>f",  "mzggVG=`z"); 
--- /\--- replaced by LSP formating
+vim.keymap.set("n", "<leader>f",  "mzggVG=`z");
 
 require "rg.mappings.telescope"
 require "rg.mappings.nvim-tree"
