f06576c93c46ac2eff2fbe052f0a1b8436bd8d1f
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-04-25 05:07:33 +0200

    Harpoon shortcuts now use <leader>1-4 instead of <leader>h1-4

--- lua/rg/mappings/harpoon.lua
+++ lua/rg/mappings/harpoon.lua
@@ -5,9 +5,9 @@ local term = require("harpoon.term")
 vim.keymap.set('n', '<leader>hm', function() mark.add_file() end)
 vim.keymap.set('n', '<leader>hh', function() ui.toggle_quick_menu() end)
 
-vim.keymap.set('n', '<leader>h1', function() ui.nav_file(1) end)
-vim.keymap.set('n', '<leader>h2', function() ui.nav_file(2) end)
-vim.keymap.set('n', '<leader>h3', function() ui.nav_file(3) end)
-vim.keymap.set('n', '<leader>h4', function() ui.nav_file(4) end)
+vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
+vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
+vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
 
 vim.keymap.set('n', '<leader>ht', function() term.gotoTerminal(1) end)
