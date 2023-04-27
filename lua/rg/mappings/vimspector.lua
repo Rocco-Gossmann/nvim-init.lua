06e6997f61a06904bde6f5d96dece277e1d16f33
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-04-27 05:06:51 +0200

    vimspector replaced <s-f8> with <f9> moved <f9> and <f10> to <f10> and <f11>

--- lua/rg/mappings/vimspector.lua
+++ lua/rg/mappings/vimspector.lua
@@ -3,8 +3,8 @@ vim.keymap.set('n', '<F5>', ':call vimspector#Launch()<CR>')
 
 vim.keymap.set('n', '<F7>', ':call vimspector#StepIInto()<CR>')
 vim.keymap.set('n', '<F8>', ':call vimspector#StepIOver()<CR>')
-vim.keymap.set('n', '<S-F8>', ':call vimspector#StepIOut()<CR>')
-vim.keymap.set('n', '<F9>', ':call vimspector#Continue()<CR>')
+vim.keymap.set('n', '<F9>', ':call vimspector#StepIOut()<CR>')
+vim.keymap.set('n', '<F10>', ':call vimspector#Continue()<CR>')
 
-vim.keymap.set('n', '<F10>', ':call vimspector#Stop()<CR>:call vimspector#Reset()<CR>')
+vim.keymap.set('n', '<F11>', ':call vimspector#Stop()<CR>:call vimspector#Reset()<CR>')
 
