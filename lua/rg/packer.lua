613e2a14e99f7fe07568e44bfdb830b492ee7614
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-04-28 06:05:12 +0200

    tweakted lense.vim height

--- lua/rg/packer.lua
+++ lua/rg/packer.lua
@@ -23,6 +23,8 @@ return require('packer').startup(function(use)
             let g:lens#disabled_filetypes = ['NvimTree', 'nerdtree', 'fzf']
             let g:lens#width_resize_min = 130
             let g:lens#width_resize_max = 130
+            let g:lens#height_resize_min = 30 
+            let g:lens#height_resize_max = 30 
         ]]
     }
 
