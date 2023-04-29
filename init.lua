244be18ee92f8aee196551dd688afaa59ec4f84d
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-04-29 04:52:09 +0200

    Added the option to create a vim configuration per CWD

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
 require "rg"
+
+local cwd = vim.fn.getcwd()
+local filename = cwd.."/.nviminit.lua";
+local file = vim.fn.findfile(filename)
+
+if file ~= '' then
+    dofile(file);
+else
+    print("you can customize NVIM further for this folder by creating a '" .. filename .. "' file");
+end
+
--- lua/rg/mappings/harpoon.lua
+++ lua/rg/mappings/harpoon.lua
@@ -1,6 +1,5 @@
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
-local term = require("harpoon.term")
 
 vim.keymap.set('n', '<leader>hm', function() mark.add_file() end)
 vim.keymap.set('n', '<leader>hh', function() ui.toggle_quick_menu() end)
@@ -9,5 +8,9 @@ vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
 vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
 vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
+vim.keymap.set('n', '<leader>5', function() ui.nav_file(5) end)
+vim.keymap.set('n', '<leader>6', function() ui.nav_file(6) end)
+vim.keymap.set('n', '<leader>7', function() ui.nav_file(7) end)
+vim.keymap.set('n', '<leader>8', function() ui.nav_file(8) end)
+vim.keymap.set('n', '<leader>9', function() ui.nav_file(9) end)
 
-vim.keymap.set('n', '<leader>ht', function() term.gotoTerminal(1) end)
