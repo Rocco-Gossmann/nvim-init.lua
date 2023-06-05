ae27d54e8fa9bbf1b96ea94ab95de71606c9f42d
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-06-05 05:21:35 +0200

    Better Sorting in NVIMTree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -14,33 +14,40 @@ local WIDTH_RATIO = 0.5
 
 -- Better Sorting: Numbers first names are sorted case insensitive
 local function natural_cmp(left, right)
+
+    local lt = left.type;
+    local rt = right.type;
+
     left = left.name:lower()
     right = right.name:lower()
 
-    if left == right then
+    if (lt ~= rt) then
+        local l = string.sub(lt, 0, 1);
+        local r = string.sub(rt, 0, 1);
+        return l < r
+    elseif left == right then
         return false
-    end
+    else
+        for i = 1, math.max(string.len(left), string.len(right)), 1 do
+            local l = string.sub(left, i, -1)
+            local r = string.sub(right, i, -1)
 
-    for i = 1, math.max(string.len(left), string.len(right)), 1 do
-        local l = string.sub(left, i, -1)
-        local r = string.sub(right, i, -1)
+            if type(tonumber(string.sub(l, 1, 1))) == "number" and type(tonumber(string.sub(r, 1, 1))) == "number" then
+                local l_number = tonumber(string.match(l, "^[0-9]+"))
+                local r_number = tonumber(string.match(r, "^[0-9]+"))
 
-        if type(tonumber(string.sub(l, 1, 1))) == "number" and type(tonumber(string.sub(r, 1, 1))) == "number" then
-            local l_number = tonumber(string.match(l, "^[0-9]+"))
-            local r_number = tonumber(string.match(r, "^[0-9]+"))
-
-            if l_number ~= r_number then
-                return l_number < r_number
+                if l_number ~= r_number then
+                    return l_number < r_number
+                end
+            elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
+                return l < r
             end
-        elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
-            return l < r
         end
     end
 end
 
 
 require("nvim-tree").setup({
-
     sort_by = function(nodes) table.sort(nodes, natural_cmp) end,
 
     view = {
