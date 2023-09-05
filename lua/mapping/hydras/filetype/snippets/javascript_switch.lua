2201047cd9f2176aebd742b112ac8a50c91444f6
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-09-05 05:59:40 +0200

    Javascript Switch-Snippts must be invoked from normal mode

--- lua/mapping/hydras/filetype/snippets/javascript_switch.lua
+++ lua/mapping/hydras/filetype/snippets/javascript_switch.lua
@@ -1,24 +1,22 @@
 local Hydra = require("hydra")
 
 return Hydra({
-    mode = { "i" },
-    body = "§",
     hint = [[ TS/JS Snippet => Switch
 ========================
    _s_ => switch() {}
    _c_ => case     _d_ => default
 ]],
     heads = {
-        { "s", "switch(<esc>mza){[NXT]}<esc>`za" },
-        { "c", "<esc>ocase <esc>mza: [NXT]; break;<esc>`za" },
-        { "d", "<esc>odefault:<esc>mza break;<esc>`za" },
+        { "s", "aswitch(<esc>mza){[NXT]}<esc>`za" },
+        { "c", "ocase <esc>mza: [NXT]; break;<esc>`za" },
+        { "d", "odefault<esc>mza: break;<esc>`za" },
         { "<esc>", nil, {exit=true} },
         { "q", nil, {exit=true} }
     },
     config = {
         color="teal",
 		invoke_on_body = true,
-		hint = { position="bottom", border="rounded" },
+		hint = { position="middle", border="rounded" },
         buffer = vim.api.nvim_get_current_buf()
     }
 })
