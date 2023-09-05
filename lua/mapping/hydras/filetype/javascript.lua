179bd886e829a7d967db7f6beca1626a98ac8ad6
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-09-05 06:28:35 +0200

    Javascript snippets are invoced via normal mode q now too

--- lua/mapping/hydras/filetype/javascript.lua
+++ lua/mapping/hydras/filetype/javascript.lua
@@ -1,13 +1,9 @@
 local Hydra = require("hydra")
 local jsSwitchHydra = require "mapping.hydras.filetype.snippets.javascript_switch"
 
-
---==============================================================================
--- Snippets Hydra
---==============================================================================
-local templatesHydra = Hydra({
-    mode = { "i" },
-    body = "§",
+return Hydra({
+    mode = { 'n' },
+    body = "q",
     hint = [[ JS Snippets: 
 =========================
    _f_ => function
@@ -15,45 +11,16 @@ local templatesHydra = Hydra({
 
 ]],
     heads = {
-        { 'f', 'function <esc>mzi([NXT]) {[NXT]}<esc>`zi ' },
-        { 's', function() jsSwitchHydra:activate() end , {exit=true}},
-        { "<esc>", nil, {exit=true}},
-        { "q", nil, {exit=true}}
-    },
+        { 'f', 'afunction <esc>mzi([NXT]) {[NXT]}<esc>`zi ' },
+        { 's', function() jsSwitchHydra:activate() end },
 
+        { "<esc>", nil, {exit=true} }, { "q", nil, {exit=true} }
+    },
     config = {
         color="teal",
 		invoke_on_body = true,
-        nowait=true,
 		hint = { position="middle", border="rounded" },
         buffer = vim.api.nvim_get_current_buf()
     }
 })
 
-
-local myHydra = Hydra({
-    mode = { 'n' },
-    body = "q",
-    hint = [[ Javascript Actions:
-=================
-Insert mode
------------------
-   _§_ => Snippets
-
-]],
-    heads = {
-        {"s", function() templatesHydra:activate() end },
-        {"<esc>", nil, {exit=true}},
-        {"q", nil, {exit=true}}
-    },
-    config = {
-        color="pink",
-		invoke_on_body = true,
-		hint = { position="bottom", border="rounded" },
-        buffer = vim.api.nvim_get_current_buf()
-    }
-})
-
-myHydra.snippets = templatesHydra;
-
-return myHydra
