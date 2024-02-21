6cbe8dd68466eded02d8611ae12a58ecaf2eaa13
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2024-02-21 13:24:35 +0100

    Better Code-Folding

---
+++ lua/rg/env.lua
@@ -0,0 +1,29 @@
+local os = require "os"
+
+local home = os.getenv("HOME")
+
+--      from $HOME/.config/$NVIM_APPNAME
+local appname = os.getenv("NVIM_APPNAME")
+if appname == nil then appname='nvim' end
+
+local function doFileIfExists(fileName)
+    if vim.fn.findfile(fileName) ~= '' then
+        local ret = dofile(fileName);
+        if ret == nil then
+            return true
+        else
+            return ret
+        end
+    else
+        return nil;
+    end
+end
+
+
+return {
+    appname = appname,
+    home = home,
+    confdir = home .. "/.config/" .. appname,
+
+    doFileIfExists = doFileIfExists
+}
