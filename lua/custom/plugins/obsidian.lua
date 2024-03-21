db3e71af058fa935cbc148fd5320581fa49aa0fd
Rocco Gossmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2024-03-21 03:50:35 +0100

    open markdown urls in browser via gf

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -21,5 +21,13 @@ return {
 	},
 	opts = {
 		workspaces = require("rg.custom.obsidianworkspaces"),
+		-- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+		-- URL it will be ignored but you can customize this behavior here.
+		---@param url string
+		follow_url_func = function(url)
+			-- Open the URL in the default web browser.
+			vim.fn.jobstart({ "open", url }) -- Mac OS
+			-- vim.fn.jobstart({"xdg-open", url})  -- linux
+		end,
 	},
 }
