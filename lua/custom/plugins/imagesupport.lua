7a21ad739ead720d175f6efa89b4f99f7025d4bb
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2024-11-07 12:00:56 +0100

    some tweaks to how images are displayed

--- lua/custom/plugins/imagesupport.lua
+++ lua/custom/plugins/imagesupport.lua
@@ -7,16 +7,15 @@ return {
 			integrations = {
 				markdown = {
 					enabled = true,
-					clear_in_insert_mode = false,
+					clear_in_insert_mode = true,
 					download_remote_images = true,
 					only_render_image_at_cursor = false,
-					filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+					filetypes = { "markdown", "vimwiki", "todo" }, -- markdown extensions (ie. quarto) can go here
 				},
 				neorg = {
 					enabled = true,
 					filetypes = { "norg" },
-				},
-				typst = {
+				}, typst = {
 					enabled = true,
 					filetypes = { "typst" },
 				},
@@ -29,12 +28,12 @@ return {
 			},
 			max_width = nil,
 			max_height = nil,
-			max_width_window_percentage = nil,
-			max_height_window_percentage = 50,
+			max_width_window_percentage = 100,
+			max_height_window_percentage = 100,
 			window_overlap_clear_enabled = false,                                    -- toggles images when windows are overlapped
 			window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-			editor_only_render_when_focused = false,                                 -- auto show/hide images when the editor gains/looses focus
-			tmux_show_only_in_active_window = false,                                 -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+			editor_only_render_when_focused = true,                                 -- auto show/hide images when the editor gains/looses focus
+			tmux_show_only_in_active_window = true,                                 -- auto show/hide images in the correct Tmux window (needs visual-activity off)
 			hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
 		})
 	end
