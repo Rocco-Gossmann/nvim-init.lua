2a05480adc602769aa62a9b68146268eb2a24737
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2024-11-28 07:47:28 +0100

    Made Debugger Shortcuts same as in VSCoder

--- lua/rg/mapping_functions.lua
+++ lua/rg/mapping_functions.lua
@@ -1,9 +1,8 @@
 local telescope_builtin = require('telescope.builtin');
 local gs = package.loaded.gitsigns
 local dapui = require("dapui");
-local dap = require("dap");
 
-local debuggerRunning = false
+local debuggerUIOpen = false
 
 return {
 
@@ -34,22 +33,30 @@ return {
 
     tmuxMakeRun         = "<cmd>!tmux split-window -v -p25 \'make run ; read\'<cr>",
 
-    start_stop_debugger = function()
-        if (debuggerRunning) then
-            dapui.close(); vim.cmd.DapTerminate()
-            debuggerRunning = false
-        else
-            vim.inspect(dap);
+    start_debugger      = function()
+        if (debuggerUIOpen == false) then
             dapui.open();
-            vim.cmd.DapContinue();
-            debuggerRunning = true;
+            debuggerUIOpen = true;
         end
+
+        vim.cmd.DapContinue();
     end,
 
-    debugger_evaluate = function()
+    stop_debugger = function()
+
+        if (debuggerUIOpen) then
+            dapui.close();
+            debuggerUIOpen = false;
+        end
+
+        vim.cmd.DapTerminate()
+
+    end,
+
+    debugger_evaluate   = function()
         dapui.eval();
     end,
 
-    tmuxLazyGit= "<cmd>!tmux new-window \'lazygit\'<cr>",
+    tmuxLazyGit         = "<cmd>!tmux new-window \'lazygit\'<cr>",
 
 }
