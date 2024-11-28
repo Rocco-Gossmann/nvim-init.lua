ae51238acfb1fc2aa112c92cae00f201e4aefedd
Rocco Goßmann <rocco.gossmann@facilioo.de>
2024-11-28 08:01:18 +0100

    TaskRunner: Moved tasks into `rg.custom.tasks`

--- lua/custom/plugins/nvim-taskrunner.lua
+++ lua/custom/plugins/nvim-taskrunner.lua
@@ -3,55 +3,8 @@ local M = {
 	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
 	init = function()
 
-		require("nvim-taskrunner").setup({
+		require("nvim-taskrunner").setup( require("rg.custom.tasks") )
 
-			{
-				label = "New Bookmark Comment",
-				action = "o//78a=o BM: o78a=kA ",
-			},
-
-			{
-				label = "Go return err nil",
-
-				action = "oif err != nil {oreturn erro}o",
-			},
-
-			{
-				label = "CPP: Create NEW C++ - .h + .cpp file combo",
-				action = function()
-					vim.ui.input({ prompt = "What's it called? " },
-						function(classname)
-							local tr = classname:gsub("^%s+", ""):gsub("%s+$", "");
-							if #tr > 0 then
-								local norm = tr:lower():gsub("%s+", "_")
-								local up = norm:upper()
-
-								vim.ui.input({
-									prompt = "Where is it located? ",
-									default = vim.fn.getcwd()
-								}, function(loc)
-									local trl = loc:gsub("^%s+", ""):gsub("%s+$", ""):gsub("/+$", "");
-
-									-- TODO: Needs Path validation / creation
-
-									if #trl > 0 then
-										local hcmd = "i#ifndef " .. up .. "_Ho#define " .. up .. "_Ho#endif2OO";
-										vim.cmd.tabnew(trl .. "/" .. norm .. ".h")
-										vim.cmd.normal(hcmd .. "")
-
-										local ccmd = "i#include \"./" .. norm .. ".h\"2o";
-										vim.cmd.vs(trl .. "/" .. norm .. ".cpp")
-										vim.cmd.normal(ccmd .. "")
-
-										-- TODO: Needs Output, when invalid path is given
-									end
-								end)
-							end
-						end)
-				end
-			},
-
-		});
 	end,
 };
 
