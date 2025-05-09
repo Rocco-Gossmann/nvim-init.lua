34b0cbc2fc98af3ef875e2fe39333ddb1a2068d1
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2025-05-09 06:32:24 +0200

    theme: transparent background

--- init.lua
+++ init.lua
@@ -735,6 +735,14 @@ require('lazy').setup({
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
+      require("rose-pine").setup({
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = true,
+        },
+      })
+
       vim.cmd 'colorscheme rose-pine'
     end,
   },
