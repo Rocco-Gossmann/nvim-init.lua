4333aa57b1b71257428956e07f4c9db468cef005
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2025-10-03 19:18:08 +0200

    feat: adding pwd to lua includePath

--- lua/rg/plugins/lsp.lua
+++ lua/rg/plugins/lsp.lua
@@ -154,6 +154,12 @@ return {
 							completion = {
 								callSnippet = "Replace",
 							},
+
+							workspace = {
+								vim.fn.getcwd() .. "/lua",
+								vim.fn.getcwd(),
+							},
+
 							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
 							-- diagnostics = { disable = { 'missing-fields' } },
 						},
