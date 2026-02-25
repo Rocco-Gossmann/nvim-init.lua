ca7bb6d3bd50667d4dd1d11b330c50c67fd26535
Rocco Goßmann <rocco.gossmann@facilioo.de>
2026-02-25 10:39:13 +0100

    refactor: propper intelephense setup

--- lua/rg/plugins/lsp.lua
+++ lua/rg/plugins/lsp.lua
@@ -168,6 +168,10 @@ return {
 						},
 					},
 				},
+
+				intelephense = {
+
+				}
 			}
 
 			local ensure_installed = vim.tbl_keys(servers or {})
@@ -203,6 +207,55 @@ return {
 						},
 					}
 				},
+			})
+
+			vim.lsp.config("intelephense", {
+
+				init_options = {
+					licenceKey = "/opt/licenses/intelephense.txt",
+				},
+
+				settings = {
+					intelephense = {
+
+						environment = {
+							includePaths = {
+								"/var/lib/phpunit",
+							},
+						},
+
+						diagnostics = {
+							enable = true,
+							argumentCount = true,
+							deprecated = true,
+							duplicateSymbols = true,
+							embeddedLanguages = true,
+							implementationErrors = true,
+							languageConstraints = true,
+							memberAccess = false,
+							noMixedTypeCheck = true,
+							relaxedTypeCheck = true,
+							run = "onType",
+							typeErrors = true,
+							undefinedClassConstants = true,
+							undefinedConstants = true,
+							undefinedFunctions = true,
+							undefinedMethods = true,
+							undefinedProperties = true,
+							undefinedSymbols = true,
+							undefinedTypes = true,
+							undefinedVariables = true,
+							unexpectedTokens = true,
+							unusedSymbols = true,
+						},
+
+						inlayHint = {
+							returnTypes = true
+						}
+
+					},
+				},
+
 			})
 
 			require("mason-lspconfig").setup({
