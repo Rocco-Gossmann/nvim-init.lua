278cefc702566bdf1a635662f795fc86fa6a2658
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2026-02-25 17:31:27 +0100

    refactor:  kicking out stylua

--- lua/rg/plugins/lsp.lua
+++ lua/rg/plugins/lsp.lua
@@ -176,7 +176,9 @@ return {
 
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"stylua",
+				"intelephense",
+				"php-debug-adapter",
+				"jq"
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
