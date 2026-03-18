596af743470e1b3a93487fa508023f5c4e21e13f
Rocco Goßmann <rocco.gossmann@facilioo.de>
2026-03-18 14:49:26 +0100

    refactor: cleaning some more neovide stuff

--- init.lua
+++ init.lua
@@ -11,16 +11,3 @@ require "rg.tweaks"
 
 require("rg.env").doFileIfExists("./.nvim/init.lua");
 
-if vim.g.neovide then
-
-	vim.api.nvim_set_current_dir(vim.env.PWD)
-
-	vim.g.neovide_scale_factor = 1.0
-	vim.o.guifont="JetBrainsMono Nerd Font Mono:h16"
-
-	vim.g.neovide_opacity = 0.85
-	vim.g.neovide_window_blurred = true
-
-	vim.g.neovide_input_macos_option_key_is_meta = true
-
-end
