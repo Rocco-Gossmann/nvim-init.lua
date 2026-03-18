a60f4d6f68960aea0b5429f5969bdce1e48fb012
Rocco Goßmann <rocco.gossmann@facilioo.de>
2026-03-18 11:35:12 +0100

    feat: Markdown "Peek" commands

--- lua/rg/plugins/codingutils.lua
+++ lua/rg/plugins/codingutils.lua
@@ -44,7 +44,18 @@ return {
 		opts = {
 			processor = "magick_cli",
 		}
-	}
+	},
+
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
 
 
 }
