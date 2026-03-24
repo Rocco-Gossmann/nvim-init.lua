5428d551aebf1d8ecab95ccdf5402493c0439b53
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2026-03-24 08:30:44 +0100

    feat: autoinstalling marksman and lua_ls

--- lua/rg/plugins/lsp.lua
+++ lua/rg/plugins/lsp.lua
@@ -263,7 +263,7 @@ return {
 			})
 
 			require("mason-lspconfig").setup({
-				ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+				ensure_installed = {"marksman", "lua_ls"}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
 				automatic_installation = false,
 				handlers = {
 					function(server_name)
