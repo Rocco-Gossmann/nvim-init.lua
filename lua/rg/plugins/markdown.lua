c8893dc87892fcc4c5548dddcbb5edb77811daba
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2026-03-31 06:43:42 +0200

    tweaking markdown codeblock displays

--- lua/rg/plugins/markdown.lua
+++ lua/rg/plugins/markdown.lua
@@ -106,6 +106,102 @@ return {
 				scope_priority = nil,
 			},
 
+			code = {
+				-- Turn on / off code block & inline code rendering.
+				enabled = true,
+				-- Additional modes to render code blocks.
+				render_modes = false,
+				-- Turn on / off sign column related rendering.
+				sign = true,
+				-- Whether to conceal nodes at the top and bottom of code blocks.
+				conceal_delimiters = true,
+				-- Turn on / off language heading related rendering.
+				language = true,
+				-- Determines where language icon is rendered.
+				-- | center | center of code block |
+				-- | right  | right of code block  |
+				-- | left   | left of code block   |
+				position = 'left',
+				-- Whether to include the language icon above code blocks.
+				language_icon = true,
+				-- Whether to include the language name above code blocks.
+				language_name = true,
+				-- Whether to include the language info above code blocks.
+				language_info = true,
+				-- Amount of padding to add around the language.
+				-- If a float < 1 is provided it is treated as a percentage of available window space.
+				language_pad = 1,
+				-- A list of language names for which rendering will be disabled.
+				disable = {"markdown"},
+				-- A list of language names for which background highlighting will be disabled.
+				-- Likely because that language has background highlights itself.
+				-- Use a boolean to make behavior apply to all languages.
+				-- Borders above & below blocks will continue to be rendered.
+				disable_background = { 'diff' },
+				-- Width of the code block background.
+				-- | block | width of the code block  |
+				-- | full  | full width of the window |
+				width = 'block',
+				-- Amount of margin to add to the left of code blocks.
+				-- If a float < 1 is provided it is treated as a percentage of available window space.
+				-- Margin available space is computed after accounting for padding.
+				left_margin = 0,
+				-- Amount of padding to add to the left of code blocks.
+				-- If a float < 1 is provided it is treated as a percentage of available window space.
+				left_pad = 2,
+				-- Amount of padding to add to the right of code blocks when width is 'block'.
+				-- If a float < 1 is provided it is treated as a percentage of available window space.
+				right_pad = 2,
+				-- Minimum width to use for code blocks when width is 'block'.
+				min_width = 0,
+				-- Determines how the top / bottom of code block are rendered.
+				-- | none  | do not render a border                               |
+				-- | thick | use the same highlight as the code body              |
+				-- | thin  | when lines are empty overlay the above & below icons |
+				-- | hide  | conceal lines unless language name or icon is added  |
+				border = 'thin',
+				-- Used above code blocks to fill remaining space around language.
+				language_border = '█',
+				-- Added to the left of language.
+				language_left = '█',
+				-- Added to the right of language.
+				language_right = '',
+				-- Used above code blocks for thin border.
+				above = '▄',
+				-- Used below code blocks for thin border.
+				below = '▀',
+
+				-- Turn on / off inline code related rendering.
+				inline = false,
+				-- Icon to add to the left of inline code.
+				inline_left = '',
+				-- Icon to add to the right of inline code.
+				inline_right = '',
+				-- Padding to add to the left & right of inline code.
+				inline_pad = 0,
+				-- Priority to assign to code background highlight.
+				priority = 140,
+				-- Highlight for code blocks.
+				highlight = 'RenderMarkdownCode',
+				-- Highlight for code info section, after the language.
+				highlight_info = 'RenderMarkdownCodeInfo',
+				-- Highlight for language, overrides icon provider value.
+				highlight_language = nil,
+				-- Highlight for border, use false to add no highlight.
+				highlight_border = 'RenderMarkdownCodeBorder',
+				-- Highlight for language, used if icon provider does not have a value.
+				highlight_fallback = 'RenderMarkdownCodeFallback',
+				-- Highlight for inline code.
+				highlight_inline = 'RenderMarkdownCodeInline',
+				-- Determines how code blocks & inline code are rendered.
+				-- | none     | { enabled = false }                           |
+				-- | normal   | { language = false }                          |
+				-- | language | { disable_background = true, inline = false } |
+				-- | full     | uses all default values                       |
+				style = 'full',
+			},
+
+
 		},
 	},
 
@@ -150,7 +246,7 @@ return {
 			},
 			external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
 
-			ui = { enable = false },
+			-- ui = { enable = false },
 			diagnostic = { enabled = false },
 
 			workspaces = vim.g.obsidianworkspaces,
