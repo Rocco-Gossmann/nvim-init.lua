fa3441d13f7620579ac56687d51accea27768fbb
Rocco Goßmann <rocco.gossmann@facilioo.de>
2026-04-01 14:30:22 +0200

    cleanup:

--- lua/rg/plugins/treesitter.lua
+++ lua/rg/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 return { -- Highlight, edit, and navigate code
 	'nvim-treesitter/nvim-treesitter',
+	lazy = false,
 	build = ':TSUpdate',
-	-- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
 	opts = {
 		ensure_installed = {
 			'bash',
