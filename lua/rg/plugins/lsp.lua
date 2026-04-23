653f1a1eb85172301bbb685a175f53587c21641b
Rocco Goßmann <rocco.gossmann@facilioo.de>
2026-04-23 10:22:36 +0200

    feat: diagnostics info in file

--- lua/rg/plugins/lsp.lua
+++ lua/rg/plugins/lsp.lua
@@ -25,6 +25,46 @@ require("mini.completion").setup({
   },
 
 })
+
+
+vim.diagnostic.config({
+
+	severity_sort = true,
+
+	float = { border = "rounded", source = "if_many" },
+
+	underline = { severity = vim.diagnostic.severity.ERROR },
+
+	signs = vim.g.have_nerd_font and {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+	} or {},
+
+	virtual_lines = true,
+
+	-- virtual_text = {
+	-- 	source = "if_many",
+	-- 	spacing = 2,
+	-- 	format = function(diagnostic)
+	-- 		local diagnostic_message = {
+	-- 			[vim.diagnostic.severity.ERROR] = diagnostic.message,
+	-- 			[vim.diagnostic.severity.WARN] = diagnostic.message,
+	-- 			[vim.diagnostic.severity.INFO] = diagnostic.message,
+	-- 			[vim.diagnostic.severity.HINT] = diagnostic.message,
+	-- 		}
+	-- 		return diagnostic_message[diagnostic.severity]
+	-- 	end,
+	-- },
+})
+
+
+
+
+
 -- vim.pack.add({
 -- 	"https://github.com/L3MON4D3/LuaSnip",
 -- 	"https://github.com/folke/lazydev.nvim",
@@ -189,34 +229,6 @@ end
 --	end,
 -- })
 
-
--- vim.diagnostic.config({
---
--- 	severity_sort = true,
--- 	float = { border = "rounded", source = "if_many" },
--- 	underline = { severity = vim.diagnostic.severity.ERROR },
--- 	signs = vim.g.have_nerd_font and {
--- 		text = {
--- 			[vim.diagnostic.severity.ERROR] = "󰅚 ",
--- 			[vim.diagnostic.severity.WARN] = "󰀪 ",
--- 			[vim.diagnostic.severity.INFO] = "󰋽 ",
--- 			[vim.diagnostic.severity.HINT] = "󰌶 ",
--- 		},
--- 	} or {},
--- 	virtual_text = {
--- 		source = "if_many",
--- 		spacing = 2,
--- 		format = function(diagnostic)
--- 			local diagnostic_message = {
--- 				[vim.diagnostic.severity.ERROR] = diagnostic.message,
--- 				[vim.diagnostic.severity.WARN] = diagnostic.message,
--- 				[vim.diagnostic.severity.INFO] = diagnostic.message,
--- 				[vim.diagnostic.severity.HINT] = diagnostic.message,
--- 			}
--- 			return diagnostic_message[diagnostic.severity]
--- 		end,
--- 	},
--- })
 
 -- local capabilities = require("blink.cmp").get_lsp_capabilities()
 
